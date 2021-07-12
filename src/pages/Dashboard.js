1ced3850c91b74411ff1c34e033eb8298380d75b
Mawushie <afi.mawushie.honyah@gmail.com>
2021-07-12 15:31:52 +0000

    log out button added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,27 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { logOutAction } from '../actions/authActions';
 import UserForm from '../components/UserForm';
 import UserList from '../components/UserList';
 
-
-
 class Dashboard extends React.Component{
+
+  handleLogOut = () =>{
+    this.props.logOutAction()
+  }
 
   render(){
     return (
-      <div className="row">
-        <div className = "col-md-6 userinput">
+      <div>
+        <div className = 'row text-right'  style ={{padding : 20}}>
+          <button onClick = {this.handleLogOut}>Log Out</button>
+        </div>
+        
+        <div className="row">
+          <div className = "col-md-6 userinput">
 
-          <UserForm /> 
+            <UserForm /> 
+          </div>
+          
+          <div className = "col-md-6 userlist">
+            <UserList />   
+          </div>
+          
         </div>
-        
-        <div className = "col-md-6 userlist">
-          <UserList />   
-        </div>
-        
       </div>
   
     );
   }
 
 }
-export default Dashboard;
+
+const mapDispatchToProps = {
+  logOutAction
+}
+export default connect(null, mapDispatchToProps)(Dashboard);
